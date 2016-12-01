d80da7aa6f1db72ed276d50e3c2c4f8bc4f74d3f
Amrit Gautam <gautamamrit24@gmail.com>
2016-12-01 17:21:39 +0200

    Style the application

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
     var $placeImage = $("#placeImage");
     var $searchForm = $("#searchForm");
-    var $placeInfo = $("#placeNytInfo");
-    var $wikiInfo = $("#wikiInfo");
+    var $placeNytInfo = $("#placeNytInfo");
+    var $placeWikiInfo = $("#placeWikiInfo");
 
     $searchForm.submit(function(e){
         loadData(e);
@@ -14,41 +14,46 @@ $(document).ready(function(){
         var $streetSource = $("#street");
         var $citySource = $("#city");
         var $completeAddress = $streetSource.val() + ', ' + $citySource.val();
-        var loadingText = "<h4>Searching in New York Times articles ....</h4>";
-        // NewYork Times API request
-        var newYorkTimesURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+        var loadingTextNyt = "<h3>Searching in New York Times articles ....</h3>";
+        var loadingTextWiki = "<h3>Searching in Wikipedia ....</h3>";
+        var newYorkTimesURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";   // NewYork Times API request
+        
         newYorkTimesURL += '?'+ $.param({
             'api-key':"76bd84159cd34d57ab3169c66ade9e8a",
             'q': $streetSource.val()
         });
         
         $placeImage.empty(); // Remove previous searched image to Insert New image per search
-        $placeInfo.empty(); // Remove previous NYT news to Insert New news hedings per search
-
+        $placeNytInfo.empty(); // Remove previous NYT news to Insert New news hedings per search
+        $placeWikiInfo.empty();
         // Add image from Google Street View API of matched address
-        $placeImage.append("<img src='https://maps.googleapis.com/maps/api/streetview?size=500x300&location="+$completeAddress+"&key=AIzaSyDGxY_Mg_cOM4uNKdFe-TaBYqpBBBfxSQM'>");
+        $placeImage.append("<img id = 'searchImage' src='https://maps.googleapis.com/maps/api/streetview?size=500x300&location="+$completeAddress+"&key=AIzaSyDGxY_Mg_cOM4uNKdFe-TaBYqpBBBfxSQM'>");
         
-        $placeInfo.append(loadingText);
+        $placeNytInfo.append(loadingTextNyt);
+        $placeWikiInfo.append(loadingTextWiki);
         // API request
 
         $.getJSON(newYorkTimesURL)
             .done(function(data){
                 // console.log(data.response.docs);
-                var title = "<h4>Matched results from New York Times magazine.</h4>"
+                var title = "<h3>Matched results from New York Times magazine</h3>"
                 var matchItems = data.response.docs; // Array of matched search news in NYT
-                var outDiv = '<ul>'
+                var outDiv = '<ol>'
+                
                 matchItems.forEach(function(item){
                     outDiv += '<li><a target="_blank" href="'+item.web_url+'">'+item.headline.main+'</a><p>'+item.lead_paragraph+'</p></li>';
                 });
-                outDiv +='</ul>';
-                $placeInfo.empty(); // Remove previous NYT news to Insert New news hedings per search
-                $placeInfo.append(title);
-                $placeInfo.append(outDiv);
+                outDiv +='</ol>';
+                $placeNytInfo.empty(); // Remove previous NYT news to Insert New news hedings per search
+                $placeNytInfo.addClass("informationBox");
+                $placeNytInfo.append(title);
+                $placeNytInfo.append(outDiv);
             })
             .fail(function(err){
-                $placeInfo.empty();
-                var errorMessage = "<h4>Sorry! New York Times articles could not be loaded.</h4>";
-                $placeInfo.append(errorMessage);
+                $placeNytInfo.empty();
+                $placeNytInfo.addClass("informationBox");
+                var errorMessage = "<h3 style='color:#c83349;'>Sorry, NewYork Times articles could not be loaded.</h3>";
+                $placeNytInfo.append(errorMessage);
             });
 
         $.ajax( {
@@ -57,17 +62,31 @@ $(document).ready(function(){
             crossDomain: true
             })
             .done(function(data){
-                console.log(data);
-                var items = data.query.search;
-                items = items.slice(0, 5);
-                items.map(function(item){
-                    item.snippet = item.snippet.slice(0, 100);
-                    var htmlToAppend = "<li><a href='https://www.wikipedia.org/wiki/" + item.title + "'><h4 class='wiki-article-title'>" + item.title +"</h4><p>" + item.snippet + "...</p></a>";
-                    $wikiInfo.append(htmlToAppend);
-                });
+                // console.log(data);
+                if(!data.error){
+                    var matchItems = data.query.search;
+                    var outDiv = '<ol>';
+                    var title = '<h3>Matched results from Wikipedia</h3>';
+                    
+                    matchItems.forEach(function(item){
+                        outDiv += '<li><a target="_blank" href="https://www.wikipedia.org/wiki/'+ item.title +'">'+ item.title +'</a><p>'+item.snippet+'</p></li>';
+                    });
+                    outDiv +='</ol>';
+                    $placeWikiInfo.empty(); // Remove previous NYT news to Insert New news hedings per search
+                    $placeWikiInfo.addClass("informationBox");
+                    $placeWikiInfo.append(title);
+                    $placeWikiInfo.append(outDiv);
+                }else{
+                    $placeWikiInfo.empty();
+                    $placeWikiInfo.addClass("informationBox");
+                    $placeWikiInfo.append("<h3 style='color:#c83349;'>Failed to load wikipedia content</h3>");
+                }
+                
             })
-            .fail(function(){
-                $wikiInfo.append("<p>Failed to load wikipedia content</p>");
+            .fail(function(err){
+                $placeWikiInfo.empty();
+                $placeWikiInfo.addClass("informationBox");
+                $placeWikiInfo.append("<h3 style='color:#c83349;'>Failed to load wikipedia content</h3>");
             });
 
         // Empty search fields items in the end
